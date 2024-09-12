5a1bd36b081992c1877bfcf47001dd59f0f968c0
Diego Christie <diechristie@gmail.com>
2024-09-12 12:13:39 -0300

    compressed api into 1

--- __main__.py
+++ __main__.py
@@ -5,56 +5,23 @@ import time
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'documents'
+UPLOAD_PDF_FOLDER = 'pdf_documents'
+UPLOAD_IMAGE_FOLDER = 'image_documents'
 
-@app.route('/string_compliance_check', methods=['POST'])
-def string_compliance_check():
-    if request.method == 'POST':
-        # Extract prompt from the received JSON
-        data = request.json
-        user_input = data.get('user_input', '')
-        checklist_input = data.get('checklist_input', '')
-
-        # Process the prompt (this is where you'd integrate your AI processing logic)
-        response = process_w_string(user_input, checklist_input)
-
-        # Return the response in JSON format
-        return jsonify({'sentiment': response[0],'response': response[1]})
-
-
-@app.route('/pdf_compliance_check', methods=['POST'])
+@app.route('/ai_compliance_check', methods=['POST'])
 def pdf_compliance_check():
     if request.method == 'POST':
-        # Check if the request contains a file
-        if 'pdf_file' not in request.files:
-            return "No file part", 400
-
-        # Get the file object
-        file = request.files['pdf_file']
+        data = request.json
+        user_text_input = data.get('user_text_input', '')
+        checklist_input = data.get('checklist_input', '')
+        files_input = data.get('files_input', '')
         
-        # Get the file name
-        file_name = file.filename
+        print(files_input)
 
-        timestamp = str(int(time.time()))
-        file_name_with_timestamp = f"{timestamp}_{file_name}"
-        
-        # Read the binary data from the file
-        #pdf_binary = file.read()
-
-        checklist_input = request.form.get('checklist_input')
-        # Define the path to save the file (inside the uploads folder)
-
-        save_path = os.path.join(UPLOAD_FOLDER, file_name_with_timestamp)
-        
-        # Save the file to the specified folder
-        file.save(save_path)
-
-        
-        # Process the prompt (this is where you'd integrate your AI processing logic)
-        response = process_w_pdf(checklist_input, file_name_with_timestamp)
+        response = process_w_ai(checklist_input, user_text_input, files_input)
 
         # Return the response in JSON format
         return jsonify({'sentiment': response[0],'response': response[1]})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=8080)
--- ai.py
+++ ai.py
@@ -9,17 +9,70 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from tempfile import mkdtemp
 import shutil
 import os
+import requests
 
-model_version = "llama3.1"
+model_version = "llama3.1:70b-instruct-q5_K_M"
 
-def process_w_string(user_input, checklist_input):
-    llm = OllamaLLM(model=model_version)
-
-    response = llm.invoke("Please review the following details and determine compliance: Regulation Overview: " + checklist_input +
-                          " User Submission: " + user_input + 
-                          "Determine if the user submission meets the following specific standards set by the regulation. Include a brief explanation for your decision based on the information provided.")
+def process_w_ai(checklist_input, user_text_input, files_input):
+    pdf_folder_path = "pdf_documents/"
+    #image_folder_path = "image_documents/"
     
-    return item_approval(response)
+    download_pdfs_from_urls(files_input, pdf_folder_path)
+    
+    all_docs = []
+    for filename in os.listdir(pdf_folder_path):
+        if filename.endswith('.pdf'):
+            file_path = os.path.join(pdf_folder_path, filename)
+            loader = PyPDFLoader(file_path)
+            docs = loader.load()
+            all_docs.extend(docs)
+
+    print("CANTIDAD DE DOCUMENTOS: " + str(len(all_docs)))
+    if (len(all_docs) > 0):
+        llm = OllamaLLM(model=model_version)
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
+
+        splits = text_splitter.split_documents(all_docs)
+
+        chroma_temp_dir = mkdtemp()
+
+        vectorstore = Chroma.from_documents(documents=splits, embedding=HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5", model_kwargs = {'device':'cuda'} ), persist_directory=chroma_temp_dir)
+        
+        retriever = vectorstore.as_retriever()
+
+        system_prompt = (
+            "You are an assistant for checking compliance to certain regulations. "
+            "The following pieces of context are the user's submission.  "
+            "Your job is to determine if the user submission meets the specific standards set by the regulation."
+            "\n\n"
+            "User written input: " + user_text_input +
+            "User input through PDF files: " + "{context}"
+        )
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system_prompt),
+                ("human", "{input}"),
+            ]
+        )
+
+        question_answer_chain = create_stuff_documents_chain(llm, prompt)
+        rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+        results = rag_chain.invoke({"input": "Give a justification for your answer. The regulation for comparing with the user submission (context) is the following: " + checklist_input})
+
+        shutil.rmtree(chroma_temp_dir, ignore_errors=True)
+        cleanup_documents_folder(pdf_folder_path)
+
+        return item_approval(results['answer'])
+    
+    else:
+        llm = OllamaLLM(model=model_version)
+
+        response = llm.invoke("Please review the following details and determine compliance: Regulation Overview: " + checklist_input +
+                            " User Submission: " + user_text_input + 
+                            "Determine if the user submission meets the following specific standards set by the regulation. Include a brief explanation for your decision based on the information provided.")
+        
+        return item_approval(response)
 
 def item_approval(response):
     llm = OllamaLLM(model=model_version)
@@ -44,48 +97,6 @@ def parse_response(response):
 
     return decision
 
-def process_w_pdf(checklist_input, pdf_filename):
-    file_path = "documents/" + pdf_filename
-    print("PRINTING PDF NAME AND PATH: " + file_path)
-    loader = PyPDFLoader(file_path)
-    docs = loader.load()
-
-    llm = OllamaLLM(model=model_version)
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-
-    splits = text_splitter.split_documents(docs)
-
-    chroma_temp_dir = mkdtemp()
-
-    vectorstore = Chroma.from_documents(documents=splits, embedding=HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5", model_kwargs = {'device':'cuda'} ), persist_directory=chroma_temp_dir)
-    
-    retriever = vectorstore.as_retriever()
-
-    system_prompt = (
-        "You are an assistant for checking compliance to certain regulations. "
-        "The following pieces of context are the user's submission.  "
-        "Your job is to determine if the user submission meets the specific standards set by the regulation. "
-        "\n\n"
-        "{context}"
-    )
-    print("3")
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", system_prompt),
-            ("human", "{input}"),
-        ]
-    )
-
-    question_answer_chain = create_stuff_documents_chain(llm, prompt)
-    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
-    results = rag_chain.invoke({"input": "Give a justification for your answer. The regulation for comparing with the user submission (context) is the following: " + checklist_input})
-
-    shutil.rmtree(chroma_temp_dir, ignore_errors=True)
-    cleanup_documents_folder('documents/')
-
-    return item_approval(results['answer'])
-
 def cleanup_documents_folder(directory):
     #Delete all files in the specified directory.
     for filename in os.listdir(directory):
@@ -97,3 +108,23 @@ def cleanup_documents_folder(directory):
                 shutil.rmtree(file_path)  # Remove the directory and its contents
         except Exception as e:
             print(f'Failed to delete {file_path}. Reason: {e}')
+            
+def download_pdfs_from_urls(url_list, folder_path):
+    """
+    Downloads PDF files from a list of URLs and saves them to the given folder.
+    """
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    
+    for i, url in enumerate(url_list):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                file_path = os.path.join(folder_path, f"document_{i+1}.pdf")
+                with open(file_path, 'wb') as f:
+                    f.write(response.content)
+                print(f"Downloaded: {file_path}")
+            else:
+                print(f"Failed to download {url} (status code: {response.status_code})")
+        except Exception as e:
+            print(f"Error downloading {url}: {e}")
